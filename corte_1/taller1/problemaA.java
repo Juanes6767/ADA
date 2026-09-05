b46c297e3536ffc1360ede659a8f4005856f5d60
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-05 12:39:22 -0500

    Implement problem A solution in problemaA.java

---
+++ corte_1/taller1/problemaA.java
@@ -0,0 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Problema A");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("N:");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            int izq = 0;
+            for (int j = 1; j <= i-1; j++) {
+                izq = izq + j;
+            }
+            int der = 0;
+            for (int j = i+1; j <= N; j++) {
+                der = der + j;
+            }
+            if (izq == der){ 
+                System.out.println(i);
+                System.exit(0);
+            }      
+        }
+        System.out.println("No hay");         
+    }
+}
