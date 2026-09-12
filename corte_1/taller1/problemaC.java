a39129cf3175738b284bb0cf0dbe658adcf7556d
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-11 23:29:46 -0500

    Implement Bernoulli calculation in problemaC.java

--- corte_1/taller1/problemaC.java
+++ corte_1/taller1/problemaC.java
@@ -1,1 +1,21 @@
+//Bernouilli
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int masmelo = 49999;
+        
+        int N = scanner.nextInt();
+        int P = scanner.nextInt();
 
+        int acum = 0;
+        
+        for (int i = 1; i <= N; i++) {
+            acum = acum + ((int) Math.pow(i, P) % masmelo);
+        }
+        
+        System.out.println(acum % masmelo);
+    }
+}
