f1b3929f5081e8d0802dce53872e3ca9e7ec573f
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-13 17:21:08 -0500

    starting steps problemaB.java
    Fixed import statement and added main class structure.

--- corte_1/taller1/problemaB.java
+++ corte_1/taller1/problemaB.java
@@ -1,1 +1,7 @@
-
+// Use this editor to write, compile and run your Java code online
+import java.utils.scanner
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
