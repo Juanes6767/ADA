fbe6e73ef67ae845999d175466e11d97a769f2bc
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-13 17:24:48 -0500

    primera anidación problemaB.java
    Fixed import statement and variable initialization.

--- corte_1/taller1/problemaB.java
+++ corte_1/taller1/problemaB.java
@@ -1,7 +1,17 @@
+// Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.utils.scanner
 public class Main {
     public static void main(String[] args) {
-        
+        sc scanner = new Scanner();
+
+        int M = sc.nextInt();
+
+        for (int i = 0; i<M ;i++) {
+            int izq = 0;
+            
+            
+        }
+            
     }
 }
