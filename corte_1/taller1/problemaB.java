4b63f705ac3caf130541ba44b4c2de722924cecd
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-14 16:31:46 -0500

    fuerza bruta problemaB.java
    Fixed syntax errors and improved logic in the main method.

--- corte_1/taller1/problemaB.java
+++ corte_1/taller1/problemaB.java
@@ -1,17 +1,27 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
-import java.utils.scanner
+import java.utils.scanner;
+import java.lang.math;
 public class Main {
     public static void main(String[] args) {
         sc scanner = new Scanner();
 
-        int M = sc.nextInt();
+        int M = sc.nextInt(); // COTA SUPERIOR
 
-        for (int i = 0; i<M ;i++) {
+        int acum = 0;
+        for (int i = 1; i<=M ;i++) {
             int izq = 0;
-            
-            
+            for (int j = 1; j<i ;j++) {
+                izq += math.pow(j,2);   
+            }
+            int der = 0;
+            for (int j = i+1; j<=M ;j++) {
+                izq += math.pow(j,2);   
+            }
+            if (der % izq == 0){
+                acum+=1;
+            }          
         }
-            
-    }
+        System.out.println(acum);
+        
 }
