44cb60ce1c908c450891b6427e0824e21357add7
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-14 16:50:34 -0500

    1 menos problemaB

--- corte_1/taller1/problemaB.java
+++ corte_1/taller1/problemaB.java
@@ -1,27 +1,29 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
-import java.utils.scanner;
-import java.lang.math;
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
-        sc scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
 
-        int M = sc.nextInt(); // COTA SUPERIOR
-
+        int M = scanner.nextInt(); // COTA SUPERIOR
+        
         int acum = 0;
-        for (int i = 1; i<=M ;i++) {
+        
+        for (int i = 2; i<=M-1 ;i++) {
             int izq = 0;
             for (int j = 1; j<i ;j++) {
-                izq += math.pow(j,2);   
+                izq += Math.pow(j,2);   
             }
             int der = 0;
             for (int j = i+1; j<=M ;j++) {
-                izq += math.pow(j,2);   
+                der += Math.pow(j,2);   
             }
             if (der % izq == 0){
-                acum+=1;
+                acum= acum + 1;
             }          
         }
-        System.out.println(acum);
+        System.out.println(acum-1);
         
+    }
 }
