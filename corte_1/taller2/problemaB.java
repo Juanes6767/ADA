c9d148a66935ae861bc58de7d559abc4561cd160
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-16 14:27:54 -0500

    Import Math class in problemaB.java

--- corte_1/taller2/problemaB.java
+++ corte_1/taller2/problemaB.java
@@ -1,6 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
+import java.lang.Math;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
