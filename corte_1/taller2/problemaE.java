b1d13d729b10bd7fdf0fb36d4fc67a9da85222e5
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-16 14:33:10 -0500

    Base conteo de divisores problemaE

--- corte_1/taller2/problemaE.java
+++ corte_1/taller2/problemaE.java
@@ -1,1 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextLine();
+        int count = 0;
+        for (int i = 1; i<=N;i++){
+            if (N % i == 0){
+                count += 1;
+            }
+        }
+        System.out.println(count); 
+    }
+}
