1b38c2bba9a54b9fd9bc321fc1122bf96d782063
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-16 14:22:57 -0500

    Leer linea problemaB.java
    Fix the loop to iterate through the array correctly.

--- corte_1/taller2/problemaB.java
+++ corte_1/taller2/problemaB.java
@@ -1,1 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String[] N = sc.nextLine().split(" ");
+        for (int i = 0;i<N.length;i++){
+            System.out.println(N[i]);
+        }
+    }
+}
