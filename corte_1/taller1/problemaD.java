34746232e1f3a547c3c42dfdb9971cba7a8d5f5c
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-20 09:29:34 -0500

    "" 1= '' problemaD.java

--- corte_1/taller1/problemaD.java
+++ corte_1/taller1/problemaD.java
@@ -5,33 +5,26 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        String texto = String.valueOf(N*N);
-        boolean encontrado = true;
-        boolean casoRaro = false;
-        String primero = "";
-        String segundo = "";
-        String primeros = "";
-        String segundos = "";
-        for (int i = 1; i<=N ;i++) {
-            primero = String.valueOf(i);
-            segundo = String.valueOf(N-i);
-            primeros = (texto.split("0")[0] + String.join("", Arrays.copyOfRange(texto.split("0"), 1, texto.split("0").length)) );
-            segundos =  String.join("", (Arrays.copyOfRange(texto.split("0"), 0, texto.split("0").length-1))) + texto.split("0")[texto.split("0").length-1];
-                
-            casoRaro = primeros.equals(primero + segundo)  || segundos.equals(primero + segundo) || primeros.equals(segundo+ primero) || segundos.equals(segundo+ primero);
-            
-            if ( (primero + segundo).equals(texto) || (segundo + primero ).equals(texto) || casoRaro){
-                System.out.println("KAP");
-                encontrado = true;
-                break;
-            }
-            
-            else{
-                encontrado = false;
-            }
-            }
-        if (encontrado == false){
+        boolean zero= false;
+        if (String.valueOf(N).charAt(String.valueOf(N).length()-1)=='0') {
             System.out.println("NO");
+            zero=true;
         }
+        else{
+        String texto = String.valueOf(N * N);
+
+for (int corte = 1; corte < texto.length(); corte++) {
+    String primero = texto.substring(0, corte);
+    String segundo = texto.substring(corte);
+
+    if (Integer.parseInt(primero) + Integer.parseInt(segundo) == N) {
+        System.out.println("KAP");
+        return;
+    }
+}
+        }
+if (zero == false){
+System.out.println("NO");
+}
     }
 }
