06150b4bf80655d67b2477024527c3e55dc43104
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-20 07:39:22 -0500

    "00" no se acepta problemaD.java
    Changed the initial value of 'encontrado' and modified its logic in the loop.

--- corte_1/taller1/problemaD.java
+++ corte_1/taller1/problemaD.java
@@ -6,14 +6,15 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         String texto = String.valueOf(N*N);
-        boolean encontrado = false;
+        boolean encontrado = true;
         for (int i = 1; i<=N ;i++) {
             if ( (String.valueOf(i) + String.valueOf(N-i)).equals(texto) ){
                 System.out.println("KAP");
+                encontrado = true;
                 break;
             }
             else{
-                encontrado = true;
+                encontrado = false;
             }
             }
         if (encontrado == false){
