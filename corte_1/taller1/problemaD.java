5051ef6116bf1ddb3662055f456f3ae176ec22b8
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-20 07:24:55 -0500

    descomponer en sumas problemaD.java
    Fix logic error in the loop and add missing braces.

--- corte_1/taller1/problemaD.java
+++ corte_1/taller1/problemaD.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String texto = String.valueOf(N*N);
+
+        for (int i = 1; i<=N ;i++) {
+            if ( (String.valueOf(i) + String.valueOf(N-i)).equals(texto) ){
+                System.out.println("KAP");
+            else{
+                System.out.println("NO");
+            }
+            }
+    }
+}
+}
