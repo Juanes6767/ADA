f60a4044e402bdfa1be0366d2e744cee4e3a8324
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-20 06:53:20 -0500

    reverso problemaE.java
    Implement palindrome check and count iterations.

--- corte_1/taller1/problemaE.java
+++ corte_1/taller1/problemaE.java
@@ -1,1 +1,32 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+import java.math.BigInteger;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int number = sc.nextInt();
+        BigInteger count = BigInteger.ZERO;
+        while (true){
+            int original = number;
+            int reversed = 0;
+            while (number != 0) {
+                reversed = reversed * 10 + number % 10;
+                number /= 10;
+            }
+        if (original == reversed){
+            System.out.println(count);
+            break;
+        }
+        if (count.compareTo(BigInteger.valueOf(10000000000L))==-1){
+        System.out.println("L");
+            break;
+        }
+        number = number + reversed;
+        count.add(BigInteger.ONE);
+            
+        }
+        
+    }
+}
 
