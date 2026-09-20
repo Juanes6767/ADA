59a26abfb4cfe391dc54ab4cc5769769e1807c0b
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-20 14:53:00 -0500

    Update problemaB.java

--- corte_1/taller2/problemaB.java
+++ corte_1/taller2/problemaB.java
@@ -1,14 +1,31 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
-import java.lang.Math;
+import java.util.Arrays;
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String[] N = sc.nextLine().split(" ");
-        for (int i = 0;i<N.length;i++){
-if (Integer.valueOf( N[i])%(i+1)==0) {       System.out.println(Integer.valueOf( N[i]));}
         
+        Scanner sc = new Scanner(System.in);
+        int producto = 1;
+        boolean esMultiplo = true;
+        boolean breakage = true;
+        String[] list = sc.nextLine().split(" ");
+        
+        Arrays.sort(list);
+        int mayor = Integer.valueOf(list[list.length-1]);
+        for (int i = 0;i<list.length;i++){
+            producto*= Integer.valueOf(list[i]);
+        }
+ 
+        for (int i = mayor;i<producto;i+=mayor){
+            for (int j = 0; j < list.length; j++) {
+            if (i % Integer.valueOf(list[j]) != 0 || i == Integer.valueOf(list[j])) {
+                System.out.println(i);
+                breakage= true;
+                break;
+                }
+            }
+            if (breakage){}
+            break;
     }
 }}
