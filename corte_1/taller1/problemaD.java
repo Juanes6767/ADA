9d539d379dd46875e4948de3c6c8d2ecbaba5d93
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-20 08:38:48 -0500

    .equals() problemaD.java

--- corte_1/taller1/problemaD.java
+++ corte_1/taller1/problemaD.java
@@ -13,7 +13,7 @@ class Main {
         for (int i = 1; i<=N ;i++) {
             primero = String.valueOf(i);
             segundo = String.valueOf(N-i);
-            casoRaro= (texto.split("0")[0] + Arrays.copyOfRange(texto.split("0"), 1, texto.split("0").length)  == primero + segundo) || (Arrays.copyOfRange(texto.split("0"), 0, texto.split("0").length-1) + texto.split("0")[texto.split("0").length] == primero + segundo);
+            casoRaro= ( (texto.split("0")[0] + segundo).equals(primero + segundo)) ||  ( (primero + texto.split("0")[texto.split("0").length-1]).equals( primero + segundo) );
             
             if ( (primero + segundo).equals(texto) || casoRaro){
                 System.out.println("KAP");
