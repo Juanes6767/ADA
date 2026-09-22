9fbd7f53b13badbdd2b9b57fd9339b6d81ec39de
Juan Esteban Pineda-Henao <159493494+Juanes6767@users.noreply.github.com>
2026-09-21 21:31:06 -0500

    acumulativo problemaB.java
    Refactored the main method to calculate LCM and GCD of input numbers.

--- corte_1/taller2/problemaB.java
+++ corte_1/taller2/problemaB.java
@@ -1,50 +1,31 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 class Main {
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        
-        String[] list = sc.nextLine().split(" ");
-        int producto = 1;
-        for (int i = 0;i<list.length;i++){
-            producto*= Integer.valueOf(list[i]);
-        }
-        int mayor = 0;
-        int gcd = 0;
-        int count=0;
+        sc.nextLine();
+        String[] list = sc.nextLine().trim().split("\\s+");
 
-        // Encontrar el mayor
-        for (int i = 0; i < list.length; i++) {
-            int num = Integer.valueOf(list[i]);
+        long mcm = Long.parseLong(list[0]);
 
-            if (num > mayor) {
-                mayor = num;
-            }
+        for (int i = 1; i < list.length; i++) {
+            long num = Long.parseLong(list[i]);
+            mcm = lcm(mcm, num);
         }
 
-        // Probar posibles divisores
-        for (int i = 1; i <= mayor; i++) {
-
-            boolean esGcd = true;
-
-            for (int j = 0; j < list.length; j++) {
-                int num = Integer.valueOf(list[j]);
-
-                if (num % i != 0) {
-                    esGcd = false;
-                    count++;
-                    break;
-                }
-            }
-
-            if (esGcd) {
-                gcd = i;
-                
-            }
-        }
-
-        System.out.println((int)((producto/gcd)/Math.pow(gcd,list.length-2)));
+        System.out.println(mcm);
     }
 }
